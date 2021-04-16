6f4acf1ea2dbfbbd1c2020733de3955ae09ea793
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-16 13:43:25 -0700

    Implemented style/css loaders via webpack

--- client/src/components/Star/Star.js
+++ client/src/components/Star/Star.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
-const Star = ({rating}) => {
-
+import './star.css';
+const Star = ({ rating = 3 }) => {
+  const uniCodeEmptyStar = '&#9734;';
+  const uniCodeFullStar = '9733';
+  const stars = [];
+  for (let i = 0; i < rating; i++) {
+    stars.push(uniCodeFullStar);
+  }
   return (
     <div>
-      <h1>STAR</h1>
-      <div className='star-container'>
-        <div className='star-five'></div>
-      </div>
+      {stars.map((uniCodeStar, idx) =>
+        <span key={idx}>{String.fromCodePoint(uniCodeStar)}</span>
+      )}
     </div>
   );
 };
