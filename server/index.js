afc0140ee8c394f206372dc6c2b768123e11cb27
Jason Yeh <cheshenye@gmail.com>
2021-04-16 18:53:33 -0700

    Incomplete API setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const request = require('request');
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+// let getReposByUsername = (username) => {
+//   let url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/'
+
+//   let options = {
+//     url: url,
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': `token ${config.TOKEN}`
+//     }
+//   };
+
+//   request.get(options) => {
+//     return;
+//   };
+// }
 
 app.listen(PORT, () => {
   console.log('Catwalk  listening on localhost 8080')
