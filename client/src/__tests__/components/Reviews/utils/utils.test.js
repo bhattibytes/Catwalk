c01acd7bec8bbca4c0a613b0748f0fb968f7075b
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-19 16:18:13 -0700

    Utils findAverageStars function * Created findAverageStars function to get averagae of stars * Created unit test for this feature

--- client/src/__tests__/components/Reviews/utils/utils.test.js
+++ client/src/__tests__/components/Reviews/utils/utils.test.js
@@ -4,6 +4,11 @@ import { metaDataReviews } from '../../../../dummy_data/meta-reviews.js';
 describe('Utils helper functions', () => {
   it('findAverageStars Should find the average amount of stars', () => {
     const { ratings } = metaDataReviews;
-    findAverageStars(ratings);
+    /**
+     * ratings ->  "4": "1", "5": "1" => (4 * 1) + (5 * 1) = 9
+     * 9 / ratings.length (2) = 4.5
+     */
+    const expectedAverage = 4.5;
+    expect(findAverageStars(ratings)).toBe(expectedAverage);
   });
 });
--- client/src/components/Reviews/Ratings/utils/index.js
+++ client/src/components/Reviews/Ratings/utils/index.js
@@ -1,10 +1,25 @@
 /**
  * @param {ratings} obj
- * findAverageStars takes in an object of ratings
- * and returns that average number
+ * findAverageStars takes in an object of ratings and returns that average number.
+ * Sum the number of stars times its weight, and then divide it through by the total
+ * number of reviews
  */
 function findAverageStars(ratings) {
-
+  const keys = Object.keys(ratings);
+  const numOfRatings = keys.length;
+  let totalNumberOfReviews = 0;
+  let totalSum = 0;
+  for (let key of keys) {
+    // Get rating
+    const rating = Number(key);
+    // Get the weight of rating (how many times user chose this rating)
+    const weight = Number(ratings[key]);
+    // Calculate the amount of total reviews by weight
+    totalNumberOfReviews += weight;
+    // Calculate the total sum of (rating * weight)
+    totalSum += rating * weight;
+  }
+  return totalSum / totalNumberOfReviews;
 };
 
 function findAverageReviews() {
