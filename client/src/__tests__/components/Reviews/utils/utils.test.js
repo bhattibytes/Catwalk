f1eab1d4e23e0c5ce3f4f5b5b0295e1742b136d6
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-19 16:18:13 -0700

    Utils findAverageStars function * Created findAverageStars function to get averagae of stars * Created unit test for this feature

--- client/src/__tests__/components/Reviews/utils/utils.test.js
+++ client/src/__tests__/components/Reviews/utils/utils.test.js
@@ -1,9 +1,23 @@
-import { findAverageStars, findAverageReviews } from '../../../../components/Reviews/Ratings/utils';
+import { findAverageStars, findAverageRecommend } from '../../../../components/Reviews/Ratings/utils';
 import { metaDataReviews } from '../../../../dummy_data/meta-reviews.js';
 
 describe('Utils helper functions', () => {
+  /**
+    * ratings ->  "4": "1", "5": "1" => (4 * 1) + (5 * 1) = 9
+    * 9 / 2 (ratings.length) = 4.5
+  */
   it('findAverageStars Should find the average amount of stars', () => {
     const { ratings } = metaDataReviews;
-    findAverageStars(ratings);
+    const expectedAverage = 4.5;
+    expect(findAverageStars(ratings)).toBe(expectedAverage);
+  });
+  /**
+   * recommend -> "false": "1", "true": "1" => 1 + 1 = 2
+   * 2 (amount of total recommends) / 1 (amount of true)= 0.5 (50%)
+   */
+  it('findAverageReviews Should find the average amount of recommendations', () => {
+    const { recommended } = metaDataReviews;
+    const expectedRecommend = .50;
+    expect(findAverageRecommend(recommended)).toBe(expectedRecommend);
   });
 });
--- client/src/components/Reviews/Ratings/utils/index.js
+++ client/src/components/Reviews/Ratings/utils/index.js
@@ -1,14 +1,37 @@
 /**
  * @param {ratings} obj
- * findAverageStars takes in an object of ratings
- * and returns that average number
+ * findAverageStars takes in an object of ratings and returns that average number.
+ * Sum the number of stars times its weight, and then divide it through by the total
+ * number of reviews
  */
 function findAverageStars(ratings) {
+  const keys = Object.keys(ratings);
+  const numOfRatings = keys.length;
+  let totalNumberOfReviews = 0;
+  let totalSum = 0;
+  for (let key of keys) {
+    // Get rating
+    const rating = Number(key);
+    // Get the weight of rating (how many times user chose this rating)
+    const weight = Number(ratings[key]);
+    // Calculate the amount of total reviews by weight
+    totalNumberOfReviews += weight;
+    // Calculate the total sum of (rating * weight)
+    totalSum += rating * weight;
+  }
+  return totalSum / totalNumberOfReviews;
+};
+/**
+ * @param {recommended} obj
+ * findAverageRecommend takes in an object of recommended and returns the average amount
+ * of users who recommend the product. This object has properties "true" & "false". Based upon
+ * these two factors, we calculate the total sum of "true" recommendations and divide by the total amount
+ * of recommendations(true + false).
+ */
+function findAverageRecommend(recommended) {
+  const totalRecommendations = Number(recommended['true']) + Number(recommended['false']);
+  return Number(recommended['true']) / totalRecommendations;
 
 };
 
-function findAverageReviews() {
-
-};
-
-export { findAverageStars, findAverageReviews };
+export { findAverageStars, findAverageRecommend };
