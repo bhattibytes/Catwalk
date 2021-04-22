7f75e4e988731627a0a844015ffb8ef501e8cf59
bhattibytes <bhattibytes@gmail.com>
2021-04-22 00:44:34 -0400

    Start - AddBorder to Selected Thumbnail

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ThumbnailGallery from './ThumbnailGallery.js';
 import MainImageView from './MainImageView.js';
 import ProductInfo from './ProductInfo.js';
+import $ from 'jquery';
 
 class Products extends React.Component {
   constructor() {
@@ -10,6 +11,7 @@ class Products extends React.Component {
       thumbNailImages: ['https://cdn.shopify.com/s/files/1/0015/6611/3861/products/13c3447174e077f86b8c140ea9d174f1_180x.jpg', 'https://cdn.shopify.com/s/files/1/0015/6611/3861/products/a56f2d740fc6595b186b085167b9496e_180x.jpg', 'https://cdn.shopify.com/s/files/1/0015/6611/3861/products/3eb4ef35474562e5a3eef2dabde35284_180x.jpg', 'https://cdn.shopify.com/s/files/1/0015/6611/3861/products/132e7bcbb9e4e7ef7241213f588af9a5_180x.jpg'],
       fullSizeImage: ['https://cdn.shopify.com/s/files/1/0015/6611/3861/products/13c3447174e077f86b8c140ea9d174f1_2400x.jpg', 'https://cdn.shopify.com/s/files/1/0015/6611/3861/products/a56f2d740fc6595b186b085167b9496e_2400x.jpg', 'https://cdn.shopify.com/s/files/1/0015/6611/3861/products/3eb4ef35474562e5a3eef2dabde35284_2400x.jpg', 'https://cdn.shopify.com/s/files/1/0015/6611/3861/products/132e7bcbb9e4e7ef7241213f588af9a5_2400x.jpg'],
       selected: null,
+      thumbBorder: null
     };
     this.show = this.show.bind(this);
     this.fowardButton = this.fowardButton.bind(this);
@@ -17,6 +19,11 @@ class Products extends React.Component {
   }
 
   show(e) {
+    $('img').removeClass('show-border');
+    $(e.currentTarget).addClass('show-border');
+    this.setState({
+      thumbBorder: e.currentTarget
+    });
     var thumb = this.state.thumbNailImages
     for (var i = 0; i < thumb.length; i++) {
       if (thumb[i] === e.currentTarget.src) {
@@ -27,13 +34,20 @@ class Products extends React.Component {
     }
   }
 
+  setBorder() {
+
+  }
+
   fowardButton(e) {
     e.preventDefault();
+    $('img').removeClass('show-border');
+    var images = this.state.thumbNailImages;
     var fullImg = this.state.fullSizeImage;
     var current = this.state.selected;
     if (current === fullImg[fullImg.length - 1]) {
       this.setState({
-        selected: fullImg[0]
+        selected: fullImg[0],
+        thumbBorder: images[0]
       });
       return;
     }
@@ -67,8 +81,10 @@ class Products extends React.Component {
   }
 
   componentDidMount () {
+
     this.setState({
-      selected: this.state.fullSizeImage[0]
+      selected: this.state.fullSizeImage[0],
+      thumbBorder: this.state.thumbNailImages[0]
     })
   }
 
@@ -80,7 +96,7 @@ class Products extends React.Component {
             {
               this.state.thumbNailImages.map(image => {
                 var imgid = image.split('/')[10];
-                return  <ThumbnailGallery image={image} key={imgid} click={this.show}/>
+                return  <ThumbnailGallery image={image} key={imgid} click={this.show} borderStyle={this.state.thumbBorder}/>
               })
             }
           </div>
--- client/src/components/Products/ThumbnailGallery.js
+++ client/src/components/Products/ThumbnailGallery.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
  var ThumbnailGallery = (props) => {
+   if (props.borderStyle === props.image) {
+     var classNm = 'show-border'
+   }
   return (
     <div className="thumbnail">
-      <img src={props.image} width="50" height="100" onClick={props.click}/>
+      <img src={props.image} width="50" height="100" onClick={props.click} className={classNm ? classNm : null}/>
     </div>
   )
 }
