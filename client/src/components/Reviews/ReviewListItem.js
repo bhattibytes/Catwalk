a501e6f7bd9f63707c4fa55d57ec861da2380a4d
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-22 11:59:28 -0700

    Refactored ReviewListItem component to Class component

--- client/src/components/Reviews/ReviewListItem.js
+++ client/src/components/Reviews/ReviewListItem.js
@@ -2,41 +2,70 @@ import React from 'react';
 import Star from '../Star/Star.js';
 import moment from 'moment';
 
-const ReviewListItem = ({ review }) => {
-  const photos = review.photos;
-  return (
-    <div className='review'>
-      <div className='review-star-date'>
-        <Star rating={review.rating} />
+class ReviewListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMore: false
+    }
+  }
+
+  render() {
+    const { review } = this.props;
+    const { photos } = review;
+    const { showMore } = this.state;
+
+    return (
+      <div className='review'>
+        {/* Review Head */}
+        <div className='review-star-date'>
+          <Star rating={review.rating} />
+          <div>
+            {review.reviewer_name}, {moment(review.date).format('MMMM DD, YYYY')}
+          </div>
+        </div>
+        {/* Review Body */}
+        <b className='summary'>{review.summary.slice(0, 60)}</b>
+        <p>{(!showMore && review.body.length > 250) ?
+          <div>
+            <p>{review.body.slice(0, 250) + '...'}</p>
+            <a className='more-less' onClick={(e) => this.setState({ showMore: !showMore })}>Show More</a>
+          </div>
+          :
+          <div>
+            {/* Review's thumbnails (images) */}
+            <p>{review.body}</p>
+            {(photos.length > 0) ?
+              <div className='thumbnails'>
+                {photos.map((obj, idx) =>
+                  <img key={idx} src={obj.url} className='thumbnail' />
+                )}
+              </div>
+              :
+              ""
+            }
+          </div>
+        }
+        </p>
+        {/* Review's recommend */}
+        {(review.recommend) ?
+          <div>
+            <span className='checkmark'>&#10003;</span>
+            <i>I recommend this product</i>
+          </div>
+          : ''}
+        {/* Review's response */}
+        {(review.response) ? <div> Response: {review.response} </div> : ''}
+        <div></div>
+        {/* Review's helpful */}
         <div>
-          {review.reviewer_name}, {moment(review.date).format('MMMM DD, YYYY')}
+          <p>Helpful? <b>Yes</b>({review.helpfulness}) | <b>Report</b></p>
         </div>
+        <hr />
       </div>
-      <h4>{review.body.slice(0, 60)}...</h4>
-      <p>{review.summary}</p>
-      {(review.recommend) ?
-        <div>
-          <span className='checkmark'>&#10003;</span>
-          <i>I recommend this product</i>
-        </div>
-        : ''}
-      <p><b>Response: </b>{review.response}</p>
-      {(photos.length > 0) ?
-        <div className='thumbnails'>
-          {photos.map((obj, idx) =>
-            <img key={idx} src={obj.url} className='thumbnail' />
-          )}
-        </div>
-        :
-        ""
-      }
-      <div></div>
-      <div>
-        <p>Helpful? <b>Yes</b>({review.helpfulness}) | <b>Report</b></p>
-      </div>
-      <hr />
-    </div>
-  )
+    )
+
+  }
 };
 
 export default ReviewListItem;
