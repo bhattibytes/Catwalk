39d7b2a24641baa66a55f7bdfcf46dfedf4d2ade
Jason Yeh <cheshenye@gmail.com>
2021-04-23 17:25:06 -0700

    carousel added

--- client/src/components/Related/YourOutfit/YourOutfitCards.js
+++ client/src/components/Related/YourOutfit/YourOutfitCards.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {useEffect, useContext, useState} from 'react';
 import ProductCard from './ProductCard.js'
 import styles from './relatedItems.module.css'
 import AddOutfit from './AddOutfit.js'
 import { dummyProductStyles, dummyFeature } from './dummyData.js'
 
 const YourOutfitCards = (props) => {
+   // track left most card index
+   const [leftIndex, setLeftIndex] = useState(0);
+
+   // track relative movement of carousel to get true index
+   const [movement, setMovement] = useState(0);
+
+   let relatedItems = props.products;
+   let displayedItems = relatedItems.slice(leftIndex, leftIndex + 3);
+
+   // onclick function for right arrow button
+   let nextItem = () => {
+     setLeftIndex(leftIndex + 1);
+     setMovement(movement + 1);
+   };
+
+   // onclick function for left arrow button
+   let previousItem = () => {
+     setLeftIndex(leftIndex === 0 ? leftIndex - 0 : leftIndex - 1);
+     setMovement(movement === 0 ? movement - 0 : movement - 1);
+   };
 
   return (
-    // How to map mutiple arrays ?
     <div id={styles.relatedProductsContainer}>
-      <AddOutfit />
-      {props.products.map((product, index) => {
-        return <ProductCard product={product} image={dummyProductStyles.results[1].photos[0].thumbnail_url} feature={dummyFeature} key={index} />
-      })}
-    </div>
+    {leftIndex === 0 ? <div></div> : <button className={styles.carouselButton} onClick={previousItem}><i className="fas fa-angle-double-left"></i></button>}
+    <AddOutfit />
+    {displayedItems.map((product, index) => {
+      return <ProductCard product={product} image={dummyProductStyles.results[1].photos[0].thumbnail_url} feature={dummyFeature} key={index} />
+    })}
+    {leftIndex === relatedItems.length - 3 ?
+      null : <button className={styles.carouselButton} onClick={nextItem}><i className="fas fa-angle-double-right"></i></button>
+    }
+  </div>
   )
 };
 
