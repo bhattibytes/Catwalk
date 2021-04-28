10978fcd6b40a593cc5c36ec684e74944b79a716
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-27 22:31:25 -0700

    Created api server rendering for products api

--- server/routes/products/products.route.js
+++ server/routes/products/products.route.js
@@ -1,5 +1,41 @@
-var express = require('express')
-var router = express.Router()
-console.log('product router');
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const github_token = process.env.GIT_HUB_TOKEN;
+const atelierUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp';
+
+// Router for getting all products
+router.get('/', async (req, res) => {
+  try {
+    const atelierResponse = await axios({
+      method: 'GET',
+      url: `${atelierUrl}/products`,
+      headers: {
+        Authorization: github_token
+      }
+    });
+    res.status(200).json({ success: true, results: atelierResponse.data });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ success: false, message: 'Failed getting products.' });
+  }
+});
+
+// Router for getting all products styles
+router.get('/:product_id/styles', async (req, res) => {
+  const { product_id } = req.params;
+  try {
+    const atelierResponse = await axios({
+      method: 'GET',
+      url: `${atelierUrl}/products/${product_id}/styles`,
+      headers: {
+        Authorization: github_token
+      }
+    });
+    res.status(200).json({ success: true, data: atelierResponse.data.results });
+  } catch (err) {
+    res.status(500).json({ success: false, message: `Failed getting styles for product id ${product_id}` });
+  }
+});
 
 module.exports = router;
