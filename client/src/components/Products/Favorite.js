e3a1592c084608b959032067f30cf2c651cee29b
bhattibytes <bhattibytes@gmail.com>
2021-04-29 18:15:48 -0400

    Update AddToBag and Favorite

--- client/src/components/Products/Favorite.js
+++ client/src/components/Products/Favorite.js
@@ -2,18 +2,22 @@ import React from 'react';
 import $ from 'jquery';
 
  var Favorite = () => {
-  var addedToFav = (e) => {
-    e.preventDefault();
-    if ($(e.currentTarget).hasClass('favAdd')) {
-     $(e.currentTarget).removeClass('favAdd');
-    } else {
-     $(e.currentTarget).addClass('favAdd');
-    }
+
+  var mouseEnter = (e) => {
+    $(e.target).addClass('bag-hover');
   }
+
+  var mouseOut = (e) => {
+  $(e.target).removeClass('bag-hover');
+  }
+
+  var clickFavorite = (e) => {
+    console.log('favorite was clicked!')
+  }
+
   return (
-    <div className="favorite addFavOrBag">
-      <button><img src="https://apps.shopifycdn.com/listing_images/decd3084187b00ba26d5b77e532be5fe/icon/508c4711cd21aebc727137e8ad9a80f2.png" width="100px" height="100px" onClick={addedToFav}/></button>
-      <h3 className="labelFav">Favorite Item</h3>
+    <div className="favorite">
+      <button className="fav-button" onMouseEnter={mouseEnter} onMouseOut={mouseOut} onClick={clickFavorite}>FAVORITE</button>
     </div>
   )
 }
