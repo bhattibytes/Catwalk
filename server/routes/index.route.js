8e641e1234ae11bfdef520b14ad5877632e21622
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-29 11:22:02 -0700

    Updated product router for related data

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -3,10 +3,8 @@ const router = express.Router()
 
 const productsRouter = require('./products/products.route.js');
 const reviewsRouter = require('./reviews/reviews.route.js');
-const relatedRouter = require('./related/related.route.js');
 
 router.use('/products', productsRouter);
 router.use('/reviews', reviewsRouter);
-router.use('/related', relatedRouter);
 
 module.exports = router;
