a1c338fb6a8cdc49744d16513e53c9bfa3bc1c17
Marco Canchola <marcocanchola2020@gmail.com>
2021-04-29 09:42:24 -0700

    Created related router for api requests

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const productsRouter = require('./products/products.route.js');
 const reviewsRouter = require('./reviews/reviews.route.js');
+const relatedRouter = require('./related/related.route.js');
 
 router.use('/products', productsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/related', relatedRouter);
 
 module.exports = router;
